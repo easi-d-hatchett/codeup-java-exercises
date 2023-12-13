f96f89c2fd7d885d793d49d131c67c126f21005c
Easi Hatchett <easi.hatchett3@gmail.com>
2023-12-13 10:52:58 -0500

    Hello world exr

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,54 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("hello, World!");
+
+        int myFavoriteNumber = 13;
+        System.out.println(myFavoriteNumber);
+
+        String myString = "Go hard or go home!";
+        System.out.println(myString);
+        // myString = ' E';
+        // myString = 3.14159;
+        System.out.println(myString);
+
+        float myNumber;
+       // myNumber = 3.14;
+        // myNumber = 123L;
+        myNumber = 123;
+        System.out.println(myNumber);
+
+        int x = 5;
+        System.out.println(x++);
+        System.out.println(++x);
+        System.out.println(x);
+
+        int classNum= 3;
+
+       // String theNumberThree = "three";
+        //Object o = theNumberThree;
+        // int three = (int) o;
+
+       // int three = (int) "three";
+
+        //int x = 4;
+        x += 5;
+
+        //int x = 3;
+        //int y = 4;
+      //  y *= x;
+
+       // int x = 10;
+       // int y = 2;
+      //  x /= y;
+        y -= x;
+
+        System.out.println(x);
+        System.out.println(y);
+
+        int bigInt = Interger.MAX_VALUE +1;
+        System.out.println(bigInt);
+
+
+
     }
 }
---
+++ src/TypesVariablesSyntaxLecture.java
@@ -0,0 +1,14 @@
+public class TypesVariablesSyntaxLecture {
+    public static void main(String[] args) {
+        System.out.println("this is an example of a statement.");
+        System.out.println("Notice that it ends with a semicolon.");
+        System.out.println("When we create statements: they MUST end with a semicolon");
+
+        byte smallNum =3;
+        System.out.println(smallNum);
+        smallNum = 127;
+        smallNum++;
+        System.out.println(smallNum);
+
+    }
+}
