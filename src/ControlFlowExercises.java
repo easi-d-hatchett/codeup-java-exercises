09e30641e6ddd3d782fb52f8185f376bd99bb7a7
Easi Hatchett <easi.hatchett3@gmail.com>
2023-12-14 11:57:49 -0500

    ControlFLow do while loop

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,3 +1,35 @@
 public class ControlFlowExercises {
+    public static void main(String[] args) {
 
-}
+
+//   // int i = 5;
+//
+//
+//
+//        int i = 1;
+//
+//
+//        while (i <= 15) {
+//            System.out.println("i is: " + i);
+//
+//            i++;
+//        }
+//
+//        System.out.println("complete.");
+
+//        int i = 0;
+//        do {
+//            System.out.println(i);
+//            i += 2;
+//        } while (i <= 100);
+
+//
+        int i = 2;
+        do {
+            System.out.println(i);
+            i *= i;
+        } while (i <= 10000);
+        }
+
+    }
+
